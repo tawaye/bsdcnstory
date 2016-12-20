6d6e38afde24a4c3bdbadb8eae590acdef812145
Tawayee <tawayee@gmail.com>
2016-12-20 20:06:59 +0000

    more fields in createnotice

--- chinesestory/models.py
+++ chinesestory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Current_Notice(models.Model):
+	notice_number = models.IntegerField(primary_key=True)
+	gathering_date = models.CharField(max_length = 12)
+	gathering_starttime = models.CharField(max_length = 12)
+	gathering_endtime = models.CharField(max_length = 12)
+	gathering_place = models.CharField(max_length = 60)
+	max_groupsize = models.IntegerField(default=15)
+	gathering_moderator = models.CharField(max_length = 20)
+	gathering_topic = models.CharField(max_length = 40)
+	registration_date = models.CharField(max_length = 12)
+	registration_time = models.CharField(max_length = 12)
--- chinesestory/views.py
+++ chinesestory/views.py
@@ -9,11 +9,10 @@ import os.path
 from datetime import datetime
 import time
 import json
+from chinesestory.models import *
 
 # Create your views here.
 
-current_notice = {}
-active_notice = '2016-12-31'
 notice_file_path = '/home/tawayee/6677ba-env/sixsevenba/chinesestory/noticefiles/' 
 
 def adminlogin(request):
@@ -49,30 +48,43 @@ def adminlogout(request):
 
 
 def createnotice(request):
+	current_notice = {}
+	context = {}
 	if not request.user.is_authenticated:
 		return render(request, 'notloggedin.html')
 	elif request.POST:
-		context = {}
 		if 'previewnotice' in request.POST:
+			context['isadmin'] = True
+			context['isactive'] = True	
 			gathering_date = request.POST['gathering_date']
 			if checkdate(gathering_date):
 				context['isadmin'] = True
+				context['isactive'] = True
 				current_notice['gathering_date'] = gathering_date
-				current_notice['gathering_place'] = 'xxx'
+				current_notice['gathering_starttime'] = request.POST['gathering_starttime']
+				current_notice['gathering_endtime'] = request.POST['gathering_endtime']
+				current_notice['gathering_place'] = request.POST['gathering_place']
+				current_notice['gathering_moderator'] = request.POST['gathering_moderator']
+				current_notice['gathering_topic'] = request.POST['gathering_topic']
+				current_notice['max_groupsize'] = request.POST['max_groupsize']
+				current_notice['registration_date'] = request.POST['registration_date']
+				current_notice['registration_time'] = request.POST['registration_time']
+				record_current_notice(current_notice)
 				context.update(current_notice)
 				return render(request, 'notice.html', context)
 			else:
 				context['errmsg'] = 'Invalid date.'
 				return render(request, 'error.html', context)
 		elif 'publishnotice' in request.POST:
+			current_notice = read_current_notice()
 			gathering_date = current_notice['gathering_date']
 			notice_file = notice_file_path + gathering_date + '.json'
 			pFile = open(notice_file, 'w')
 			pFile.write(json.dumps(current_notice))
 			pFile.close()
-			active_notice = current_notice['gathering_date']
 			return render(request, 'succeed.html', {})
 		elif 'modifynotice' in request.POST:
+			current_notice = read_current_notice()
 			return render(request, 'createnotice.html', current_notice) 
 		else:
 			return render(request, 'error.html', {})
@@ -82,12 +94,14 @@ def createnotice(request):
 def shownotice(request):
 	context = {}
 	context['isadmin'] = False
-	if active_notice != '' and datetime.strptime(active_notice, '%Y-%m-%d').date() >= datetime.today().date():
+	current_notice = read_current_notice()
+	gathering_date = current_notice['gathering_date']
+	if gathering_date != '' and datetime.strptime(gathering_date, '%Y-%m-%d').date() >= datetime.today().date():
 		context['isactive'] = True
 		context.update(current_notice)
 		return render(request, 'notice.html', context)
 	else:
-		context['errmsg'] = 'There is no active notice' + active_notice
+		context['errmsg'] = 'There is no active notice' + gathering_date 
 		return render(request, 'notice.html', context)
 
 
@@ -100,4 +114,52 @@ def checkdate(date):
 			return False
 	except ValueError:
 		return False
+	
+
+def record_current_notice(notice):
+	current_notice = Current_Notice(
+		notice_number = 6677,
+		gathering_date = notice['gathering_date'],
+		gathering_starttime = notice['gathering_starttime'],
+		gathering_endtime = notice['gathering_endtime'],
+		gathering_place = notice['gathering_place'],
+		max_groupsize = notice['max_groupsize'],
+		gathering_moderator = notice['gathering_moderator'],
+		gathering_topic = notice['gathering_topic'],
+		registration_date = notice['registration_date'],
+		registration_time = notice['registration_time'],
+		
+	)
+
+	current_notice.save()
+	
+	
+def read_current_notice():
+	current_notice = {}
+	count = Current_Notice.objects.all().count()
+	if count >= 1:
+		noticeDB = Current_Notice.objects.all()[0]
+		current_notice['gathering_date'] = noticeDB.gathering_date
+		current_notice['gathering_starttime'] = noticeDB.gathering_starttime
+		current_notice['gathering_endtime'] = noticeDB.gathering_endtime
+		current_notice['gathering_place'] = noticeDB.gathering_place
+		current_notice['max_groupsize'] = noticeDB.max_groupsize
+		current_notice['gathering_moderator'] = noticeDB.gathering_moderator
+		current_notice['gathering_topic'] = noticeDB.gathering_topic
+		current_notice['registration_date'] = noticeDB.registration_date
+		current_notice['registration_time'] = noticeDB.registration_time
+		
+	else:
+		current_notice['gathering_date'] = ''
+		current_notice['gathering_starttime'] = ''
+		current_notice['gathering_endtime'] = ''
+		current_notice['gathering_place'] = ''
+		current_notice['gathering_moderator'] = ''
+		current_notice['gethering_topic'] = ''
+		current_notice['registration_date'] = ''
+		current_notice['registration_time'] = ''
+	
+	return current_notice
+	
+	
 	
